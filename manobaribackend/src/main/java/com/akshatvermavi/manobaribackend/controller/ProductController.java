2090867cdaece90f212984b499ff775a41757632
akshatvermavi <akshatvermanike@gmail.com>
2025-05-13 12:31:07 +0530

    Search Feature is updated

--- manobaribackend/src/main/java/com/akshatvermavi/manobaribackend/controller/ProductController.java
+++ manobaribackend/src/main/java/com/akshatvermavi/manobaribackend/controller/ProductController.java
@@ -99,6 +99,13 @@ public class ProductController {
         }
 
     }
+    @GetMapping("/product/search")
+    public ResponseEntity<List<Product>> searchProducts(@RequestParam String keyword) {
+
+        List<Product> products = service.searchProducts(keyword);
+        System.out.println("searching with " + keyword);
+        return new ResponseEntity<>(products, HttpStatus.OK);
+    }
 
 
 }
--- manobaribackend/src/main/java/com/akshatvermavi/manobaribackend/repo/ProductRepo.java
+++ manobaribackend/src/main/java/com/akshatvermavi/manobaribackend/repo/ProductRepo.java
@@ -2,8 +2,18 @@ package com.akshatvermavi.manobaribackend.repo;
 
 import com.akshatvermavi.manobaribackend.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface ProductRepo extends JpaRepository<Product, Integer> { // class you are working with and primary key
+    // jPQL-
+    @Query("SELECT p from Product p WHERE " +
+            "LOWER(p.name) LIKE LOWER(CONCAT('%', :keyword, '%')) OR " +
+            "LOWER(p.description) LIKE LOWER(CONCAT('%', :keyword, '%')) OR " +
+            "LOWER(p.brand) LIKE LOWER(CONCAT('%', :keyword, '%')) OR " +
+            "LOWER(p.category) LIKE LOWER(CONCAT('%', :keyword, '%'))")
+    List<Product> searchProducts(String keyword);
 
 }
 
